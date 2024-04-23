fd41ec727b23896604be03272461c52bb480521d
bruno <bruno.costa218@etec.sp.gov.br>
2024-04-23 20:19:27 -0300

    crud completo

--- app.js
+++ app.js
@@ -1,42 +1,67 @@
-let livrosList = document.querySelector('#book-list');
-function renderbook(doc) {
-    let li = document.createElement('li');
-    let titulo = document.createElement('span');
-    let autor = document.createElement('span');
-
-    li.setAttribute('data-id', doc.id);
-    titulo.textContent = doc.data().titulo;
-    autor.textContent = doc.data().autor;
-
-    //adicionando dos dados de autor e titulo na tag
-
-    li.appendChild(titulo);
-    li.appendChild(autor);
-    livrosList.appendChild(li);
+const livrolist = document.querySelector('#book-list')
+ 
+function renderBook(doc) {
+    //criaçao elementos html
+    let li = document.createElement('li')
+    let titulo = document.createElement('span')
+    let autor = document.createElement('span')
+    let excluir = document.createElement('div')
+    
+    excluir.textContent = 'x'
+    //carrega os dados nos elementos html
+ 
+li.setAttribute('data-id', doc.id)
+    titulo.textContent = doc.data().titulo
+    autor.textContent = doc.data().autor
+ 
+    //adicionando dados de autor e titulo na tag li
+    li.appendChild(titulo)
+    li.appendChild(autor)
+    li.appendChild(excluir)
+ 
+    //Trava açao no click do botao x pra exclusao do arquivo
+    excluir.addEventListener('click', (event)=>{
+        event.stopPropagation();
+        let id = event.target.parentElement.getAttribute('data-id');
+        // alert(id)
+        db.collection('libri-data').doc(id).delete()
+            .then(()=>{window.location.reload()});
+    }
+)
+    //adicionando o li na tag ul
+    livrolist.appendChild(li)
+    
 }
-
-const form = document.querySelector('#add-book-form');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    db.collection('app-libre-bruno')
-        .add({
+ 
+ 
+ 
+db.collection('libri-data')
+    .get()
+    .then(
+        (snapshot) => {
+// console.log(snapshot.docs)
+snapshot.docs.forEach(doc => {
+                console.log(doc.data())
+                renderBook(doc)      
+            });
+        }
+    )
+ 
+ 
+    //insert de livros e autores
+    const form = document.querySelector('#add-book-form')
+ 
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        db.collection('libri-data').add({
             autor: form.autor.value,
-            titulo: form.titulo.value,
+            titulo: form.titulo.value
         })
-        .then(() => {
+        .then(()=>{
             form.autor.value = '';
             form.titulo.value = '';
             window.location.reload();
-        });
-});
-
-db.collection('app-libre-bruno')
-    .get()
-    .then((snapshot) => {
-        snapshot.docs.forEach((doc) => {
-            console.log(doc.data);
-            renderbook(doc);
-        });
+        })
     });
+ 
+ 
